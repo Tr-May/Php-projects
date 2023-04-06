c20d5857f8c1142225e973d7292475ff282f46ef
Tr-May <may.passionateteacher@gmail.com>
2023-04-07 00:12:21 +0630

    deleted the unnecessary comments

--- views/navigation.php
+++ views/navigation.php
@@ -9,16 +9,3 @@ $nav = "
 	</li>
 </ul>
 ";
-// $nav = "
-// <ol class='breadcrumb'>
-//     <li class='breadcrumb-item'><a href='index.php?page=gallery'>Gallery</a></li>
-//     <li class='breadcrumb-item active' aria-current='page'><a href='index.php?page=uploadForm'>Upload new image</a></li>
-//   </ol>
-// ";
-
-// $nav = "
-// <nav class='py-3'>
-// <a href='index.php?page=gallery'>Gallery</a>
-// <a href='index.php?page=uploadForm'>Upload new image</a>
-// </nav>
-// ";
